45c35af33733b8b94779d9ed7069dc4b9e6ea159
Dino-Zunic <168399562+Dino-Zunic@users.noreply.github.com>
2025-03-05 13:15:27 +0100

    Update syscall_c.h

--- h/syscall_c.h
+++ h/syscall_c.h
@@ -1,6 +1,6 @@
-#ifndef NOVI_PROJEKAT_SYSCALL_C_H
-#define NOVI_PROJEKAT_SYSCALL_C_H
+#ifndef SYSCALL_C_H
+#define SYSCALL_C_H
 
 #include "syscall_c.hpp"
 
-#endif //NOVI_PROJEKAT_SYSCALL_C_H
+#endif // SYSCALL_C_H
